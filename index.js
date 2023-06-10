57fc6c954fb78932fd3ca06fbc59bb27e2974497
Ashley Frank <ashley.frank@ufl.edu>
2023-06-10 11:55:13 -0400

    added variables to access HTML

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+var wordToGuessEL = document.getElementById('word-to-guess')
+var previousWordEl =  document.getElementById ('previous-word')
+var incorrectLettersEl = document.getElementById ('incorrect-letters')
+var remainingGuessesEl =  document.getElementById ('remaining-guesses')
+var scoreEL =  document.getElementById ('score')
+var winsEL = document.getElementById ('wins')
+var lossesEL = document.getElementById ('losses')
+
