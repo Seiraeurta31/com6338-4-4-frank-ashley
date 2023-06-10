507aafe891b4112b7f872dfb87517cedf42322e7
Ashley Frank <ashley.frank@ufl.edu>
2023-06-10 18:02:11 -0400

    set win/loss reset

--- index.js
+++ index.js
@@ -25,14 +25,20 @@ var incorrectLettersArray = []
 var remainingGuesses = 10
 var wins = 0
 var losses = 0
+var totalLettersToSolve = 0
+
+
 
 //Selects random word from provided word array bank
-//var word = words[Math.floor(Math.random()*words.length)]
-var word = "bananas"
+var word = words[Math.floor(Math.random()*words.length)]
+//var word = "bananas"
 
 
 //loads word into a character array for later comparison
 var wordArray = Array.from(word)
+totalLettersToSolve = wordArray.length
+
+console.log(word)
 
 //loads an array = length of word, with underscores and converts to string to display to user
 for(let i=0; i<word.length; i++){
@@ -48,13 +54,11 @@ wordToGuess.textContent = (wordDisplay)
 
 
 
-
-
 document.onkeyup = function(e) {
   var key = String.fromCharCode(e.which)
   key = e.key.toLowerCase()
   var correct = false
-  var solved = false
+
 
   
   //check if key is alphanumeric
@@ -76,15 +80,15 @@ document.onkeyup = function(e) {
   }
 
 
-
 //replace underscores with valid letters as user guesses
   for(let i=0; i<wordArray.length; i++){
     if(key == wordArray[i]){
       wordDisplayArray[i] = String(key)
+      totalLettersToSolve --
       correct = true
     }
   }
-
+console.log(totalLettersToSolve)
 
 //if letter guessed is incorrect, add to inncorrect list
   if(correct === false){
@@ -95,19 +99,72 @@ document.onkeyup = function(e) {
     remainingGuesses--
   }
 
+  console.log(incorrectLettersArray)
+  console.log (incorrectLettersDisplay.textContent)
   
+
   wordToGuess.textContent = wordDisplayArray.join('')
   incorrectLettersDisplay.textContent = incorrectLettersArray.join('')
   remainingGuessDisplay.textContent = remainingGuesses
 
+ 
 
-  
-  ///winning
-
-
-
+  //winning - loosing
+  if(totalLettersToSolve == 0 && remainingGuesses > 0){
+    console.log("win")
+    wins ++
+    console.log(wins)
+    reset(word, wins, losses)
+    return
+  }
+  else if(totalLettersToSolve > 0 && remainingGuesses == 0){
+    console.log("lost")
+    losses ++
+    console.log(losses)
+    reset(word, wins, losses)
+    return
+  }
 
 }
 
+function reset(lastWord, win, loss){
+  wordToGuess = document.getElementById('word-to-guess')
+  previousWord = document.getElementById('previous-word')
+  incorrectLettersDisplay = document.getElementById('incorrect-letters')
+  remainingGuessDisplay = document.getElementById('remaining-guesses')
+  score =  document.getElementById ('score')
+  winDisplay = document.getElementById('wins')
+  lossDisplay = document.getElementById('losses')
+  wordDisplayArray = []
+  lettersGuessedArray = []
+  incorrectLettersArray = []
+  remainingGuesses = 10
+  wins = win
+  losses = loss
+  totalLettersToSolve = 0
+  previousWord.textContent = lastWord
+  console.log("reset triggered")
 
 
+  word = words[Math.floor(Math.random()*words.length)]
+  //var word = "bananas"
+
+  wordArray = Array.from(word)
+  totalLettersToSolve = wordArray.length
+
+  console.log(word)
+
+  //loads an array = length of word, with underscores and converts to string to display to user
+  for(let i=0; i<word.length; i++){
+    wordDisplayArray[i] = ('_')
+  } 
+
+  wordDisplay = wordDisplayArray.join('')
+
+  //sets starting guesses
+  remainingGuessDisplay.textContent = 10
+
+  //display underscores representing word to guess 
+  wordToGuess.textContent = (wordDisplay)
+
+}
